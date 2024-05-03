a43f977254ee47138db6cac6c147f0f7b48413d0
Ang Chen <chenang@outlook.com>
2024-05-03 22:08:35 +0800

    Add about.js

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+// Step 1: Import React. This lets you use JSX inside your .js file.
+import * as React from "react"  
+
+/* Step 2: Define your component. Note that your
+component name should start with a capital letter. */
+const AboutPage = () => {
+  return (
+    <main>
+        <h1>About Ang Chen!</h1>
+        <p>I'm a physics and computer enthusiast who loves to learn new things and create new things.
+            <br/>
+            Why not aim high and work hard?
+            <hr/>
+        </p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
+
