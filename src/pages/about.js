4c28bacfff51d9e8ff690e6c1aea941e2096fb11
Ang Chen <chenang@outlook.com>
2024-05-05 22:23:18 +0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 // Step 1: Import React. This lets you use JSX inside your .js file.
 import * as React from "react"  
+import { Link } from "gatsby"
 
 /* Step 2: Define your component. Note that your
 component name should start with a capital letter. */
@@ -7,10 +8,10 @@ const AboutPage = () => {
   return (
     <main>
         <h1>About Ang Chen!</h1>
+        <Link to="/">Back to Home</Link>
         <p>I'm a physics and computer enthusiast who loves to learn new things and create new things.
             <br/>
             Why not aim high and work hard? 
-            <hr/>
         </p>
     </main>
   )
