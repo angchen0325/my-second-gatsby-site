f010264ab1ccafd0ecef580d2cb1e66b7743b110
Ang Chen <chenang@outlook.com>
2024-05-08 07:28:53 +0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,17 @@
 // Step 1: Import React. This lets you use JSX inside your .js file.
 import * as React from "react"  
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 /* Step 2: Define your component. Note that your
 component name should start with a capital letter. */
 const AboutPage = () => {
   return (
-    <main>
-        <h1>About Ang Chen!</h1>
-        <Link to="/">Back to Home</Link>
-        <p>I'm a physics and computer enthusiast who loves to learn new things and create new things.
-            <br/>
-            Why not aim high and work hard? Try your best and achieve your goals.
-        </p>
-    </main>
+    <Layout pageTitle="About Me">
+      <p>I'm a physics and computer enthusiast who loves to learn new things and create new things.
+         <br/>
+         Why not aim high and work hard? Try your best and achieve your goals.</p>
+    </Layout>
   )
 }
 
