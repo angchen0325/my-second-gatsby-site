b48a514b18d78b9050f7ba73c151f4e07f9c6cc6
Ang Chen <chenang@outlook.com>
2024-05-07 23:28:56 +0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,7 @@ const AboutPage = () => {
         <Link to="/">Back to Home</Link>
         <p>I'm a physics and computer enthusiast who loves to learn new things and create new things.
             <br/>
-            Why not aim high and work hard? 
+            Why not aim high and work hard? Try your best and achieve your goals.
         </p>
     </main>
   )
