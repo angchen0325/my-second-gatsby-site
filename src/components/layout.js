0b5730ff21b6ad14160f3cc30864438276564167
Ang Chen <chenang@outlook.com>
2024-05-13 06:28:02 +0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,24 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { container } from './layout.module.css'
+import { 
+  container,
+  heading,
+  navlink,
+  navlinkItem,
+  navlinkText
+} from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <div className={container}>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+        <ul className={navlink}>
+          <li className={navlinkItem}><Link to="/" className={navlinkText}>Home</Link></li>
+          <li className={navlinkItem}><Link to="/about" className={navlinkText}>About</Link></li>
         </ul>
       </nav>
       <main>
-        <h1>{pageTitle}</h1>
+        <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
     </div>
